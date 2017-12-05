146e3d6fd2fb3164075ae4b147441ab5a28cd0a4
podoko <code.podoko@gmail.com>
2017-12-05 17:56:59 +0100

    initialisation championnats

--- db/content.sql
+++ db/content.sql
@@ -1,16 +1,16 @@
 INSERT INTO league (league_name) VALUES ('Ligue 1'), ('Liga BBVA'),('Bundesliga'),('Serie A'),('Première Ligue');
 
 INSERT INTO `jersey` (`jersey_name`, `jersey_desc`, `jersey_type`, `jersey_team`, `jersey_price`, `jersey_image`, `jersey_league`) VALUES
-('OL domicile', 'Maillot domicile 2015/2016', 'Domicile', 'Olympique Lyonnais', 80, 'OL-domicile.jpg', 1),
-('PSG domicile', 'Maillot domicile 2015/2016', 'Domicile', 'Paris Saint Germain', 50, 'PSG-domicile.jpg', 1),
-('Real domicile', 'Maillot domicile 2015/2016', 'Domicile', 'Real Madrid', 50, 'Real-domicile.jpg', 2),
-('Barcelone domicile','Maillot domicile 2015/2016','Domicile','FC Barcelone',70,'Barca-domicile.jpg',2),
-('Bayern Munich domicile','Maillot domicile 2015/2016','Domicile','FC Bayern Munich',60,'Munich-domicile.jpg',3),
-('Borussia Dortmund domicile','Maillot domicile 2015/2016','Domicile','BV 09 Borussia Dortmund',55,'Dortmund-domicile.jpg',3),
-('Juventus Turin domicile','Maillot domicile 2015/2016','Domicile','Juventus Turin',50,'Juventus-domicile.jpg',4),
-('Milan AC domicile','Maillot domicile 2015/2016','Domicile','Milan AC',45,'MilanAc-domicile.jpg',4),
-('Leicester City domicile','Maillot domicile 2015/2016','Domicile','Leicester City',45,'Leicester-domicile.jpg',5),
-('West Ham domicile','Maillot domicile 2015/2016','Domicile','West Ham',45,'WestHam-domicile.jpg',5);
+('OL domicile', 'Maillot domicile 2017/2018', 'Domicile', 'Olympique Lyonnais', 60, 'OL-domicile.jpg', 1),
+('PSG domicile', 'Maillot domicile 2017/2018', 'Domicile', 'Paris Saint Germain', 120, 'PSG-domicile.jpg', 1),
+('Real domicile', 'Maillot domicile 2017/2018', 'Domicile', 'Real Madrid', 50, 'Real-domicile.jpg', 2),
+('Barcelone domicile','Maillot domicile 2017/2018','Domicile','FC Barcelone',70,'Barca-domicile.jpg',2),
+('Bayern Munich domicile','Maillot domicile 2017/2018','Domicile','FC Bayern Munich',60,'Munich-domicile.jpg',3),
+('Borussia Dortmund domicile','Maillot domicile 2017/2018','Domicile','BV 09 Borussia Dortmund',55,'Dortmund-domicile.jpg',3),
+('Juventus Turin domicile','Maillot domicile 2017/2018','Domicile','Juventus Turin',50,'Juventus-domicile.jpg',4),
+('Milan AC domicile','Maillot domicile 2017/2018','Domicile','Milan AC',45,'MilanAc-domicile.jpg',4),
+('Leicester City domicile','Maillot domicile 2017/2018','Domicile','Leicester City',45,'Leicester-domicile.jpg',5),
+('West Ham domicile','Maillot domicile 2017/2018','Domicile','West Ham',45,'WestHam-domicile.jpg',5);
 
 
 
@@ -23,3 +23,6 @@ INSERT INTO user (user_name, user_last_name, user_mail, user_address, user_posta
 /* raw password is '@dm1n' */
 INSERT INTO user (user_name, user_last_name, user_mail, user_address, user_postal_code, user_city, user_password, user_salt, user_role) VALUES 
 ('admin', 'istrateur', 'admin@admin.com', '20 rue des admins', '75010', 'Paris', 'gqeuP4YJ8hU3ZqGwGikB6+rcZBqefVy+7hTLQkOD+jwVkp4fkS7/gr1rAQfn9VUKWc7bvOD7OsXrQQN5KGHbfg==', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
+
+
+-- Grilles
--- db/structure.sql
+++ db/structure.sql
@@ -1,15 +1,15 @@
-
-
 drop table if exists basket;
 drop table if exists comment;
 drop table if exists jersey;
 drop table if exists league;
 drop table if exists user;
 
+
 CREATE TABLE IF NOT EXISTS league (
   `league_id` integer not null primary key auto_increment,
   `league_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS jersey (
   `jersey_id` integer not null primary key auto_increment,
@@ -42,7 +42,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
 CREATE TABLE IF NOT EXISTS comment (
   com_id integer not null primary key auto_increment,
   com_content varchar(500) not null,
@@ -52,6 +51,7 @@ CREATE TABLE IF NOT EXISTS comment (
   constraint fk_com_usr foreign key(usr_id) references  user(user_id)
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
+
 CREATE TABLE IF NOT EXISTS basket (
   jersey_id integer not null, 
   user_id integer not null,
@@ -60,3 +60,38 @@ CREATE TABLE IF NOT EXISTS basket (
   constraint ct_basket_fk_user FOREIGN KEY(user_id) REFERENCES user(user_id),
   constraint ct_basket_pk PRIMARY KEY(jersey_id,user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Grilles concours
+drop table if exists user_grille;
+drop table if exists matchs_grille;
+drop table if exists grille;
+
+
+CREATE TABLE IF NOT EXISTS grille (
+  grille_id integer not null primary key auto_increment,
+  league_id integer not null,
+  date_fin date not null,
+  nom_grille varchar(500) not null,
+  constraint fk_grille_league foreign key(league_id) references league(league_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS matchs_grille (
+  match_id integer not null primary key auto_increment,
+  grille_id integer not null,
+  domicile varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  exterieur varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  constraint fk_matchsg_grille foreign key(grille_id) references grille(grille_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS user_grille (
+  user_id integer not null,
+  match_id integer not null,
+  result integer not null,
+  constraint fk_userg_user foreign key(user_id) references user(user_id),
+  constraint fk_userg_match foreign key(match_id) references matchs_grille(match_id),
+  constraint ct_userg_pk PRIMARY KEY(user_id,match_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
